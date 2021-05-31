68a831e10fcda1fae090868dd99aa2e43c32510a
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-05-31 20:15:08 +0530

    Checking employee attendance

--- src/empwage/empWage.java
+++ src/empwage/empWage.java
@@ -5,6 +5,14 @@ public class empWage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Welcome to Employee Wage Computation Program");
+			int isPresent=1;
+			double empCheck=Math.floor(Math.random()*10)%2;
+			if (empCheck == isPresent){
+				System.out.println("Employee is present");
+			}
+			else{
+				System.out.println("Employee is absent");
+			}
 	}
 
 }
