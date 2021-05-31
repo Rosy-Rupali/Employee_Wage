ef64871718b75d4630e4f291c8eadd1b43679f89
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-05-31 20:26:21 +0530

    added: part time & wage

--- src/empwage/empWage.java
+++ src/empwage/empWage.java
@@ -4,18 +4,28 @@ public class empWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("Welcome to Employee Wage Computation Program");
 
 
 		int isPresent=1;
 		int fullDay=8;
+		int halfDay=4;
 		int wagePerHr=20;
 		int dailyWage;
 		double empCheck=Math.floor(Math.random()*10)%2;
 		if (empCheck == isPresent){
 			System.out.println("Employee is present");
+		int fullTime=1;
+			double fullTimeCheck=Math.floor(Math.random()*10)%2;
+			if (fullTimeCheck == fullTime){
 			dailyWage=fullDay*wagePerHr;
-			System.out.println("Daily Wage of an employee: "+dailyWage);
+			System.out.println("Daily Wage of a full time employee: "+dailyWage);
+			}
+			else{
+				dailyWage=halfDay*wagePerHr;
+				System.out.println("Daily Wage of a part time employee: "+dailyWage);
+			}
 		}
 		else{
 			System.out.println("Employee is absent");
