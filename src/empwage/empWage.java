1f3725e31a5eab5cc83cab8886525f0fd9088074
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-02 05:11:06 +0530

    Refactoring of code

--- src/empwage/empWage.java
+++ src/empwage/empWage.java
@@ -1,36 +1,31 @@
 package empwage;
 
 public class empWage {
-
-	public static void main(String[] args) {
+		//constants
+		public static final	int IS_PART_TIME=1;
+		public static final	int IS_FULL_TIME=2;
+		public static final	int EMP_RATE_PER_HOUR=20;
+	
+		public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
-
-
-		int isPresent=1;
-		int fullDay=8;
-		int halfDay=4;
-		int wagePerHr=20;
-		int dailyWage;
-		double empCheck=Math.floor(Math.random()*10)%2;
-		switch ((int)empCheck){
-			case 1:System.out.println("Employee is present");
-		int fullTime=1;
-			double fullTimeCheck=Math.floor(Math.random()*10)%2;
-			if (fullTimeCheck == fullTime){
-			dailyWage=fullDay*wagePerHr;
-			System.out.println("Daily Wage of a full time employee: "+dailyWage);
-			}
-			else{
-				dailyWage=halfDay*wagePerHr;
-				System.out.println("Daily Wage of a half time employee: "+dailyWage);
-			}
-		break;
-		case 0:System.out.println("Employee is absent");
-		break;
-
-		default: System.out.println("Invalid input");
-		}
+		
+		//variables
+		int empHrs=0;
+		int empWage=0;
+		int empCheck =(int) Math.floor(Math.random()*10)%3;
+		switch(empCheck) {
+			case IS_FULL_TIME:
+					empHrs=8;
+					break;
+			case IS_PART_TIME:
+					empHrs=4;
+					break;
+			default:
+					empHrs=0;
+		}			
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: "+empWage);
 	}
 
 }
