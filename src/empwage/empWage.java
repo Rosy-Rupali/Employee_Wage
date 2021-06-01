391958b8434ed0b9bfbff2010cac7e3b167824a1
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-02 04:54:14 +0530

    Refactoring of code

--- src/empwage/empWage.java
+++ src/empwage/empWage.java
@@ -3,33 +3,23 @@ package empwage;
 public class empWage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
-
-
-		int isPresent=1;
-		int fullDay=8;
-		int halfDay=4;
-		int wagePerHr=20;
-		int dailyWage;
-		double empCheck=Math.floor(Math.random()*10)%2;
-		if (empCheck == isPresent){
-			System.out.println("Employee is present");
-		int fullTime=1;
-			double fullTimeCheck=Math.floor(Math.random()*10)%2;
-			if (fullTimeCheck == fullTime){
-			dailyWage=fullDay*wagePerHr;
-			System.out.println("Daily Wage of a full time employee: "+dailyWage);
-			}
-			else{
-				dailyWage=halfDay*wagePerHr;
-				System.out.println("Daily Wage of a part time employee: "+dailyWage);
-			}
-		}
-		else{
-			System.out.println("Employee is absent");
-		}
+		//constants
+		int IS_PART_TIME=1;
+		int IS_FULL_TIME=2;
+		int EMP_RATE_PER_HOUR=20;
+		//variables
+		int empHrs=0;
+		int empWage=0;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if(empCheck == IS_FULL_TIME)
+			empHrs=8;
+		else if(empCheck == IS_PART_TIME)
+			empHrs=4;
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: "+empWage);
 	}
 
 }
