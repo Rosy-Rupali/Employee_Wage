51c6d82e1343e11de741ecf4a6219048bab24331
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-01 15:04:57 +0530

    Refactoring the code

--- src/empwage/empWage.java
+++ src/empwage/empWage.java
@@ -6,20 +6,15 @@ public class empWage {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
 
-
-		int isPresent=1;
-		int fullDay=8;
-		int wagePerHr=20;
-		int dailyWage;
-		double empCheck=Math.floor(Math.random()*10)%2;
-		if (empCheck == isPresent){
-			System.out.println("Employee is present");
-			dailyWage=fullDay*wagePerHr;
-			System.out.println("Daily Wage of an employee: "+dailyWage);
-		}
-		else{
-			System.out.println("Employee is absent");
-		}
+		int IS_FULL_TIME=1;
+		int EMP_RATE_PER_HOUR=20;
+		int empHrs=0;
+		int empWage=0;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if(empCheck == IS_FULL_TIME)
+			empHrs=8;
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: "+empWage);
 	}
 
 }
