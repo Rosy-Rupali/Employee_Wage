4d19ba93d361dd4be864c0c01c027b429449096c
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-06-02 05:52:42 +0530

    Refactoring of code

--- src/empwage/empWage.java
+++ src/empwage/empWage.java
@@ -1,40 +1,41 @@
 package empwage;
 
 public class empWage {
+		//constants
+		public static final	int IS_PART_TIME=1;
+		public static final	int IS_FULL_TIME=2;
+		public static final	int EMP_RATE_PER_HOUR=20;
+		public static final	int NUM_OF_WORKING_DAYS=20;
+		public static final	int MAX_HRS_IN_MONTH=100;
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
+				//variables
+				int totalEmpWage=0, totalWorkingDays=0, totalEmpHrs=0;
+				
+				while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+					int empHrs=0;
+					totalWorkingDays++;
+					int empCheck =(int) Math.floor(Math.random()*10)%3;
+					switch(empCheck) {
+						case IS_FULL_TIME:
+								empHrs=8;
+								break;
+						case IS_PART_TIME:
+								empHrs=4;
+								break;
+						default:
+								empHrs=0;
+				}	
+					totalEmpHrs=totalEmpHrs+empHrs;
+					int empWage=empHrs*EMP_RATE_PER_HOUR;
+					totalEmpWage=totalEmpWage+empWage;
+					System.out.println("Emp Wage: "+empWage);
+				}
+				System.out.println("Total Employee Wage: "+totalEmpWage);
 
-
-		int isFullTime=1;
-		int isHalfTime=2;
-		int workingDaysPerMonth=20;
-		int maxHrsInMonth=100;
-		int wagePerHr=20;
-		int totalWorkingDays=0;
-		int totalEmpHrs=0;
 		
-		while(totalEmpHrs <= maxHrsInMonth || totalWorkingDays <= workingDaysPerMonth)
-		{	
-			totalWorkingDays++;
-			int attendance=(int)(Math.random()*3);
-			int empHrs=0;
-			switch(attendance) {
-			case 1: 
-				empHrs=8;
-				break;
-			case 2:
-				empHrs=4;
-				break;
-			default:
-				empHrs=0;
-				break;
-			}
-			totalEmpHrs=(totalEmpHrs+empHrs);
-		}
-		int totalSalary=(totalEmpHrs*totalWorkingDays);
-		System.out.println("Salary of an Employee: "+totalSalary);
 	}
 
 }
